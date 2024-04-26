37d1a7b507b07ce6f9d102950a5d8ec256456d4a
Joel Razuri <jrazuri98@hotmail.com>
2024-04-26 16:05:29 -0300

    add ApiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../models/product.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  baseUrl: string = 'https://fakestoreapi.com/products';
+
   constructor() { }
+
+  private _apiService  = inject(HttpClient);
+
+
+  getAllProducts(): Observable<IProduct[]> {
+    return this._apiService.get<IProduct[]>(this.baseUrl);
+  }
+
+  getProductById(id: number): Observable<IProduct> {
+    return this._apiService.get<IProduct>(`${this.baseUrl}/${id}`);
+  }
+
+  createProduct(product: IProduct): Observable<IProduct> {
+    return this._apiService.post<IProduct>(this.baseUrl, product);
+  }
+
+  updateProduct(id: number, product: IProduct): Observable<IProduct> {
+    return this._apiService.put<IProduct>(`${this.baseUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<IProduct> {
+    return this._apiService.delete<IProduct>(`${this.baseUrl}/${id}`);
+  }
+  
 }
