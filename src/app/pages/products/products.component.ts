f0066876034a05e0183da018b11a84133dfcfe84
Joel Razuri <jrazuri98@hotmail.com>
2024-04-26 22:25:41 -0300

    ProductsComponent

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { IProduct } from '../../models/product.models';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  productsList: IProduct[] = [];
+
+  constructor() {}
+
+  private _apiService = inject(ApiService);
+  
+  ngOnInit(): void {
+    this._apiService.getAllProducts().subscribe((data: IProduct[]) => {
+      this.productsList = data;
+    });
+  }
 
 }
