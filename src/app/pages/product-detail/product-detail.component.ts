25843035b9760478b6257bd345a8860dd29afd65
Joel Razuri <jrazuri98@hotmail.com>
2024-04-27 21:15:11 -0300

    ProductDetailComponent

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService)
+  public product?: IProduct;
+  loading: boolean = true;
 
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this._apiService.getProductById(params['id']).subscribe((data: IProduct) => {
+        this.product = data;
+        this.loading = false;
+      });
+    })
+  }
 }
