2b9d1ab1edf5763020820ddffa5b0e0107a5bc29
Joel Razuri <jrazuri98@hotmail.com>
2024-04-27 23:17:44 -0300

    ContactComponent

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  contactForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['',[Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  send(event: Event) {
+    event.preventDefault();
+    console.log(this.contactForm.value);
+  }
+
+  hasErrors(field: string, typeError: string) {
+    return this.contactForm.get(field)?.hasError(typeError) && this.contactForm.get(field)?.touched;
+  }
 }
